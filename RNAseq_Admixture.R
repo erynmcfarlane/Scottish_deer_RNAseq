8802cf4b2a93645ececc31c9c3aebaf25964cb3f
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2022-10-19 17:00:26 -0400

    cnvrg different analysis, only.

--- RNAseq_Admixture.R
+++ RNAseq_Admixture.R
@@ -1,19 +1,18 @@
 ### What I need to do is 1) pull all of the RNAseq data in and 2) pull all of the admixture k=2 data in
 ### then, I need to put these two data sets together, and plot a PCA of how SNP_species looks. 
 ### Think about doing MDS rather than PCA?
-### First steps can be DESeq2, just as we did in the flycatchers, using the SNP_species
-### Second step could be using limma-voom
-### Third step could be kallisto-sleuth (I think this might start from raw reads?)
-### fourth step - Harrison paper
-### fifth step - corncob loop? 
+### Gonna get rid of DESeq2 and EdgeR - a priori, they're not as good for an empirical study, and I want to pull out only the empirical stuff. And just write it.
 
+### The two questions that I'm going to ask:
+### 1) is there a difference in composition along the q gradient
+### 2) more in line with more 'traditional DEG' studies, are there DEG between red deer and hybrids, ignoring variation in q?
+### I will think more, and hard, about how to do dirichlet regression (see paper from Alex, )
 
-### only the corn cob loop will account for variation in the admixture estimates, I think. 
-
-###hoping for some quick and dirty PCAs for a 'here's what I'm doing talk' coming up
-###famous last words, I suspect I won't be able to do anything.
 
 library(pcaExplorer)
+library(CNVRG)
+library(rstan)
+library(shinystan)
 
 ### will need to add something like this to it, to only look at the protein coding genes
 read.table("~/Google Drive/Paper VI - RNAseq/Scottish_deer_RNAseq_R/ncbi_protein_coding_genes.tsv", header = T)->gene_list
@@ -70,197 +69,46 @@ colData[which(colData$Weight=="N/A"),]$Weight<-NA
 ###I'm going to take out the one sika individual, SAC00002777, and two sika like individuals SAC00002805 and SAC00002817
 
 colData_nosika<-colData[which(colData$ad_red>0.5),]
+colData_nosika$treatment<-paste(colData_nosika$SNP_species, colData_nosika$tissue) ### to make one treatment, 4 possibilities, two species and two tissues
+treatment<-colData_nosika$treatment
 count_df_coding_nosika<-count_df_coding[,c(1:22, 25:30, 33:38, 41:56)]
 
-### this isn't working - go into DESeq2
-library(DESeq2)
-library(PCAtools)
-dds <- DESeqDataSetFromMatrix(countData = count_df_coding_nosika,
-                              colData = colData_nosika, 
-                              design=  ~ as.factor(tissue)*as.factor(SNP_species)) 
-
-dds <- DESeq(dds)
-keep <- rowSums(counts(dds)) >= 3 ###I have a feeling analyses change a lot depending on this number
-dds <- dds[keep,]
-vst <- vst(dds)
-
-str(assay(vst))
-sampleDists<-dist(t(assay(vst)))
 
 
-###start with a heatmap
-library("gplots")
-library("RColorBrewer")
-library("PoiClaClu")
-poisd <- PoissonDistance(t(counts(dds)))
-samplePoisDistMatrix <- as.matrix( poisd$dd )
-rownames(samplePoisDistMatrix) <- paste( dds$SNP_species, dds$tissue, sep="-" )
-colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
-hc <- hclust(poisd$dd)
-heatmap(samplePoisDistMatrix, Rowv=as.dendrogram(hc),
-           symm=TRUE, trace="none", col=colors,
-           margins=c(2,10), labCol=FALSE )
+cnvg_data_nosika<-cbind.data.frame(treatment, t(count_df_coding_nosika))
 
-plotPCA(vst, intgroup = c("SNP_species", "tissue"))
+cnvg_data_nosika_ordered<-cnvg_data_nosika[order(cnvg_data_nosika[,1]),]
+
+###0's aren't allowed, add one to everything
+cnvg_data_nosika_ordered[,2:22927] <- 1 + cnvg_data_nosika_ordered[,2:22927]
+
+### let's make a much smaller analysis, just to start
+
+cnvg_data_nosika_short<-cnvg_data_nosika_ordered[,1:100]
 
 
-(data <- plotPCA(vst, intgroup = c( "SNP_species", "tissue"), returnData=TRUE))
-percentVar <- round(100 * attr(data, "percentVar"))
-qplot(PC1, PC2, color=SNP_species, shape=tissue, data=data) +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance"))
+###Start CNVRG analysis
 
-pca <- prcomp(t(assay(dds)))
+### run the model###
+modelOut <- cnvrg_HMC(countData = cnvg_data_nosika_short, 
+                      starts = indexer(cnvg_data_nosika_short$treatment)$starts, #c(1,39,76,113),
+                      ends = indexer(cnvg_data_nosika_short$treatment)$ends, #c(38,75,112,148),
+                      chains = 2, 
+                      burn = 500, 
+                      samples = 1000, 
+                      thinning_rate = 2,
+                      cores = 1,
+                      params_to_save = c("pi", "p"))
 
-PC1=pca$x[,1]
-PC2=pca$x[,2]
-PC3=pca$x[,3]
-PC4=pca$x[,4]
+###check convergence, as one does
 
-qplot(PC1, PC2, color=SNP_species, shape=tissue, data=data) +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance"))
+head(rstan::summary(modelOut, pars = "pi", probs =c(0.025, 0.975))$summary)
 
+#shinystan::launch_shinystan(modelOut) #to look at visualizations of diagnostic parameters
 
-mds <- data.frame(cmdscale(samplePoisDistMatrix))
-mds <- cbind(mds, colData(vst))
-qplot(X1,X2,color=SNP_species,shape=tissue,data=mds)
+point_est <- extract_point_estimate(model_out = modelOut, countData = cnvg_data_nosika_short) ## get point estimates out
 
-
-###rlog because the data are heterschedastic
-
-#rld<-rlog(dds) ### vst is already doing this - don't need this!
-
-### let's do some actual differential expression, at least between tissues?
-#dds <- DESeq(dds) - already did this, this is the analysis
-res<-results(dds) ###not really sure what this is doing, there's some pairwise thing that isn't working. 
-### just for point of view, let's use just the muscle (where we expect the difference) and look at red like and sika liek animals. 
-colData$species_like<-ifelse(colData$ad_red>0.95, 'red like', ifelse(colData$ad_red<0.5, 'sika like', 'mid hybrid'))
-plot_2A<-ggplot(colData, aes(x=reorder(deername, ad_red), y=ad_red, colour=SNP_species))+geom_point(size=1)
-plot_2A<-plot_2A+labs(x="Deer", y="Proportion Red Deer")+scale_colour_manual(values=c("purple", "red", "blue"))+theme(panel.grid=element_blank(), panel.background=element_blank(), axis.ticks.x=element_blank(), axis.text.x=element_blank(), legend.key=element_blank())
-plot2A<-plot_2A+theme(axis.line=element_line(colour="black", size=0.5))
-plot2A<-plot_2A+geom_hline(yintercept=c(0, 0.05, 0.95, 1), colour="grey")
-plot2A+guides(colour=guide_legend(title = "SNP species"))
+### differential expression
+diff_abund_test <- diff_abund(model_out = modelOut, countData = cnvg_data_nosika_short) ### this didn't work? Is it possible it only works for 2 treatments, not when I have 4? In which case, I should split up the tissues?
 
 
-#https://support.bioconductor.org/p/126713/
-#the interpretation of the log2 fold change is the fold change in expression for one unit of the variable
-
-res[which(res$padj<0.05),]
-
-### my feeling is that each tissue should be done separately, that's how deseq might work?
-
-### also going to do a volcano plot
-library('EnhancedVolcano')
-
-EnhancedVolcano(res,
-                lab = rownames(res),
-                x = 'log2FoldChange',
-                y = 'pvalue',
-                #title = 'N061011 versus N61311',
-                FCcutoff = 1.5,
-                pointSize = 3.0,
-                labSize = 6.0,
-                col=c('black', 'black', 'black', 'red3'),
-                colAlpha = 1)
-
-##### Step 2 using Limma-Voom
-
-## need to transpose the count data, and stick it on the coldata?
-### let's do the voom step first. This 'Transform count data to log2-counts per million (logCPM), estimate the mean-variance relationship and use this to compute appropriate observation-level weights. The data are then ready for linear modelling.'
-### following this tutorial
-library(edgeR)
-
-counts_edgeR<-count_df_coding_nosika
-colnames(counts_edgeR)<-as.factor(paste0(colData_nosika$deername, colData_nosika$tissue))
-do<-DGEList(counts_edgeR)
-d0<-calcNormFactors(do, method = "TMM") ### this is where I have normalized the data
-cutoff<-10
-drop<-which(apply(cpm(d0), 1, max)<cutoff)
-d<-d0[-drop,]
-dim(d) ### number of genes left
-snames<-colData_nosika$deername
-species<-colData_nosika$SNP_species
-tissue<-colData_nosika$tissue
-
-### from here, this is from https://github.com/jokelley/congen-2022/blob/main/differential_expression.md
-group<-interaction(species, tissue)
-
-plotMDS(d, col=as.numeric(group))
-y<-DGEList(counts=counts_edgeR, group=group)
-y<-calcNormFactors(y)
-
-plotMDS(y)
-
-design <- model.matrix(~0+group)
-design
-y2 <- estimateDisp(y,design) 
-
-
-fit <- glmQLFit(y2,design)
-lrt12 <- glmLRT(fit, contrast=c(1,-1,0))
-lrt13 <- glmLRT(fit, contrast=c(1,0,-1))
-lrt23 <- glmLRT(fit, contrast=c(0,1,-1))
-topTags(lrt12, n=10)
-topTags(lrt13, n=10)
-topTags(lrt23, n=10)
-
-summary(de12 <- decideTestsDGE(lrt12, p.value = 0.05))
-summary(de13 <- decideTestsDGE(lrt13, p.value = 0.05))
-summary(de23 <- decideTestsDGE(lrt23, p.value = 0.05))
-
-detags <- rownames(y)[as.logical(de)]
-plotSmear(qlf, de.tags=detags)
-abline(h = c(-1, 1), col = "blue") ### this is also a volcano plot
-
-### this gives a negative quadratic shape, which sounds bad? stack over flow basically says 'do more filtering!'
-### https://stats.stackexchange.com/questions/160255/voom-mean-variance-trend-plot-how-to-interpret-the-plot
-### I  don't entirely understand what this plot is showing, but it does seem like we want a positive quadratic, not a negatic one. 
-
-### another tutorial with more explanation of the mean-variance trend https://f1000research.com/articles/5-1408
-#### is the variance going down at the end because there are so few genes that are so highly expressed?
-### It also seems like a marked pattern like this is evidence of lots of biological variance, which is good, that's kind of what we're going for. 
-
-### might want to normalize the data a little bit more - is this getting rid of the variation across individuals? Is this what I want to do?
-### I think I need to think a lot about how there's not a ton of variation between the two axis (species and tissue) and so it's not coming out on the PCAs.
-## But that might be what's interesting here, maybe I'm looking at little amounts of variation?
-
-
-### using direchlet distribution a la Harrison et al. 2020
-library(rstan)
-library(nlme)
-options(mc.cores = parallel::detectCores())
-rstan_options(auto_write = TRUE)
-
-library(dplyr)
-bind <- function(...) cbind(...)
-
-
-
-DM <- rstan::stan_model("DM.stan", 
-                        model_name="DM")
-
-
-fitstan_HMC <-rstan::sampling(DM, 
-                              data=list("datamatrix"=count_df_coding_nosika, ### this is the count data
-                                        "nreps"=nrow(count_df_coding_nosika), 
-                                        "notus"=ncol(count_df_coding_nosika),
-                                        "N"=4, 
-                                        "start" = 1,### I don't know what this is
-                                        "end" = length(nreps), ### I don't know what this is
-                              chains=2, 
-                              control = list(max_treedepth = 15),
-                              warmup=1000, 
-                              iter=2000, 
-                              thin=2, 
-                              algorithm="NUTS", ### I don't know what this is
-                              cores=2, 
-                              seed=123,
-                              pars<-c("pi")))
-
-
-#### this is going to be way harder than I thought it would.
-
-### to get the empirical parts of this project done, going to ask two questions:
-#1) is there DEG between red deer and hybrids, using converg?
-#2) is there a difference in composition (not DEG, too hard) along the q score gradient?

85ba3482655f4673d9a292be4ad3c916cf141d0e
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2023-03-16 15:19:28 -0400

    new slurm script

--- CNVRG_slurm_beartooth.sh
+++ CNVRG_slurm_beartooth.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=RNAseq_CNVRG_short
+#SBATCH --job-name=RNAseq_CNVRG_DESeq2
 #SBATCH --nodes=2
 #SBATCH --time=7-00:00:00
 #SBATCH --account=modelscape
 #SBATCH --mem-per-cpu=20G
 
-module load swset/2018.05 gcc/7.3.0 py-scipy/1.1.0 miniconda3/4.10.3 r/4.0.5-py27
+module load arcc/1.0 gcc/12.2.0 r/4.2.2 
 
 
-R CMD BATCH /project/evolgen/emcfarl2/deer_RNAseq/scripts/RNAseq_Admixture_shortrun.R
+R CMD BATCH /project/evolgen/emcfarl2/deer_RNAseq/scripts/Streamlined_RNAseq_comparison.R
